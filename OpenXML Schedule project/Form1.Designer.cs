ad759125bb204b4d06f05500edec26f9a36b4fea
Richard <Richard@PIPPINPC>
2021-12-04 19:43:27 -0600

    Add project files.

---
+++ OpenXML Schedule project/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace OpenXML_Schedule_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstAssignments = new System.Windows.Forms.ListBox();
+            this.txtAssignment = new System.Windows.Forms.TextBox();
+            this.dtpDueDate = new System.Windows.Forms.DateTimePicker();
+            this.lblClass = new System.Windows.Forms.Label();
+            this.lblAssignment = new System.Windows.Forms.Label();
+            this.lblDueDate = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnBuild = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.cmbClass = new System.Windows.Forms.ComboBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // lstAssignments
+            // 
+            this.lstAssignments.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lstAssignments.FormattingEnabled = true;
+            this.lstAssignments.ItemHeight = 16;
+            this.lstAssignments.Location = new System.Drawing.Point(26, 100);
+            this.lstAssignments.Name = "lstAssignments";
+            this.lstAssignments.ScrollAlwaysVisible = true;
+            this.lstAssignments.Size = new System.Drawing.Size(731, 244);
+            this.lstAssignments.TabIndex = 0;
+            this.lstAssignments.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // txtAssignment
+            // 
+            this.txtAssignment.Location = new System.Drawing.Point(357, 61);
+            this.txtAssignment.Name = "txtAssignment";
+            this.txtAssignment.Size = new System.Drawing.Size(400, 23);
+            this.txtAssignment.TabIndex = 2;
+            this.txtAssignment.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // dtpDueDate
+            // 
+            this.dtpDueDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDueDate.Location = new System.Drawing.Point(232, 61);
+            this.dtpDueDate.Name = "dtpDueDate";
+            this.dtpDueDate.Size = new System.Drawing.Size(119, 23);
+            this.dtpDueDate.TabIndex = 3;
+            // 
+            // lblClass
+            // 
+            this.lblClass.AutoSize = true;
+            this.lblClass.Location = new System.Drawing.Point(26, 43);
+            this.lblClass.Name = "lblClass";
+            this.lblClass.Size = new System.Drawing.Size(34, 15);
+            this.lblClass.TabIndex = 4;
+            this.lblClass.Text = "Class";
+            // 
+            // lblAssignment
+            // 
+            this.lblAssignment.AutoSize = true;
+            this.lblAssignment.Location = new System.Drawing.Point(357, 43);
+            this.lblAssignment.Name = "lblAssignment";
+            this.lblAssignment.Size = new System.Drawing.Size(70, 15);
+            this.lblAssignment.TabIndex = 5;
+            this.lblAssignment.Text = "Assignment";
+            // 
+            // lblDueDate
+            // 
+            this.lblDueDate.AutoSize = true;
+            this.lblDueDate.Location = new System.Drawing.Point(232, 43);
+            this.lblDueDate.Name = "lblDueDate";
+            this.lblDueDate.Size = new System.Drawing.Size(55, 15);
+            this.lblDueDate.TabIndex = 6;
+            this.lblDueDate.Text = "Due Date";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(26, 381);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(139, 46);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(219, 381);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(139, 46);
+            this.btnRemove.TabIndex = 8;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnBuild
+            // 
+            this.btnBuild.Location = new System.Drawing.Point(427, 381);
+            this.btnBuild.Name = "btnBuild";
+            this.btnBuild.Size = new System.Drawing.Size(139, 46);
+            this.btnBuild.TabIndex = 9;
+            this.btnBuild.Text = "Build";
+            this.btnBuild.UseVisualStyleBackColor = false;
+            this.btnBuild.Click += new System.EventHandler(this.btnBuild_Click);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Location = new System.Drawing.Point(618, 381);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(139, 46);
+            this.btnHelp.TabIndex = 10;
+            this.btnHelp.Text = "Help";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // cmbClass
+            // 
+            this.cmbClass.FormattingEnabled = true;
+            this.cmbClass.Location = new System.Drawing.Point(26, 61);
+            this.cmbClass.Name = "cmbClass";
+            this.cmbClass.Size = new System.Drawing.Size(200, 23);
+            this.cmbClass.Sorted = true;
+            this.cmbClass.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbClass);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnBuild);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblDueDate);
+            this.Controls.Add(this.lblAssignment);
+            this.Controls.Add(this.lblClass);
+            this.Controls.Add(this.dtpDueDate);
+            this.Controls.Add(this.txtAssignment);
+            this.Controls.Add(this.lstAssignments);
+            this.Name = "Form1";
+            this.Text = "Schedule Planner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox lstAssignments;
+        private System.Windows.Forms.TextBox txtAssignment;
+        private System.Windows.Forms.DateTimePicker dtpDueDate;
+        private System.Windows.Forms.Label lblClass;
+        private System.Windows.Forms.Label lblAssignment;
+        private System.Windows.Forms.Label lblDueDate;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnBuild;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.ComboBox cmbClass;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ OpenXML Schedule project/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+
+namespace OpenXML_Schedule_project
+{
+    public partial class Form1 : Form
+    {
+        class Assignment {
+            public string whichClass { get; set; }
+            public DateTime date { get; set; }
+            public string assignment { get; set; }
+
+            public Assignment(string aClass, DateTime aDate, string anAssignemt)
+            {
+                whichClass = aClass;
+                date = aDate;
+                assignment = anAssignemt;
+            }
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Assignment> schedule = new List<Assignment>();      //hidden list of Assignment class, used to store info and to make strings for displayed list(lstAssignments).
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)    //adds the info from the class date and time fields to the list and resorts it, first by class then by date.
+        {
+            Assignment newAssignment = new Assignment(cmbClass.Text, dtpDueDate.Value, txtAssignment.Text);
+
+            if (newAssignment.assignment == "" || newAssignment.whichClass == "")
+            {
+                MessageBox.Show("Please make sure to fill out the Class and Assignment fields", "Error");
+            }
+
+            else
+            {
+                schedule.Add(newAssignment);
+
+                if (!(cmbClass.Items.Contains(cmbClass.Text)))
+                {
+                    cmbClass.Items.Add(cmbClass.Text);
+                }
+
+                schedule = schedule.OrderBy(x => x.whichClass).ThenBy(x => x.date).ToList();
+                string line;
+                lstAssignments.Items.Clear(); 
+                foreach (var plan in schedule)
+                {
+                    line = (plan.whichClass.PadRight(26));
+                    line += plan.date.ToString("MM/dd/yy").PadRight(15);
+                    line += (plan.assignment.PadRight(55));
+
+                    lstAssignments.Items.Add(line);
+                }
+                txtAssignment.Text = "";
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e) //Removes selected item from list.
+        {
+            try
+            {
+                int selectedIndex = -1;
+                selectedIndex = lstAssignments.SelectedIndex;
+                schedule.RemoveAt(selectedIndex);
+
+                schedule = schedule.OrderBy(x => x.whichClass).ThenBy(x => x.date).ToList();
+                string line;
+                lstAssignments.Items.Clear();
+                foreach (var plan in schedule)
+                {
+                    line = (plan.whichClass.PadRight(26));
+                    line += plan.date.ToString("MM/dd/yy").PadRight(15);
+                    line += (plan.assignment.PadRight(55));
+
+                    lstAssignments.Items.Add(line);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Please select an item on the list to remove.", "Error");
+            }
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)   // Display helpful information for user.
+        {
+            MessageBox.Show("First enter the class name in the Class textbox. You can reselect that class again later after adding an assignment to the list. " +
+                "\n You can choose a date using the Due Date button and enter your assigment in the Assignment box. " +
+                "\n When ready, you can click Add to add your assignment to the list. " +
+                "\n If you want to remove an item, click on the item in the list and click the remove button. " +
+                "\n When you have filled out the list with your assignments, click Build to generate your calender in Excel.", "Schedule Help");
+        }
+
+        private void btnBuild_Click(object sender, EventArgs e)  //Once all asisgnments have been entered, this asks the user if they want to make the excel file with the entered information
+        {                                                        //If they click yes, then it builds the excel file and exits the program. If no, the dialog closes.
+            try
+            {
+
+                DateTime min = schedule.Min(x => x.date);
+                DateTime max = schedule.Max(x => x.date);
+                int range = max.Subtract(min).Days + 1;
+
+                DialogResult buildResult = MessageBox.Show("Are you ready to create an Excel calendar with the given data?" +
+                    "\nYour calendar will start at: " + min.ToShortDateString() + "\n and end at: " + max.ToShortDateString() + "\n For a date range of: " + range, "Build", MessageBoxButtons.YesNo);
+                if (buildResult == DialogResult.No)
+                {
+                }
+
+                else if (buildResult == DialogResult.Yes)       // ** THIS IS WHERE THE SPREADSHEET BUILDING WILL HAPPEN **
+                {
+
+                    FolderBrowserDialog browserDialog = new FolderBrowserDialog();
+                    string filename;
+
+                    if (browserDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        filename = browserDialog.SelectedPath;
+
+                        DialogResult locationResult = MessageBox.Show("Save to: " + filename + " ?", "Build", MessageBoxButtons.OKCancel);
+
+                        if (locationResult == DialogResult.OK)
+                        {
+                            buildSpreadsheet(filename, range);
+                            MessageBox.Show("A spreadsheet calendar has been created at: " + filename);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Please ensure that there is at least one entry in the list","Error");
+            }
+        }
+
+        private void buildSpreadsheet(string fileName, int range) //builds the spreadsheet
+        {
+            fileName = fileName + "\\SpreadsheetDocumentEx.xlsx";
+
+            DateTime date = dtpDueDate.Value;
+            MessageBox.Show(date.ToString());
+
+
+            SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.
+               Create(fileName, SpreadsheetDocumentType.Workbook);
+            // Add a WorkbookPart to the document.  
+            WorkbookPart workbookpart = spreadsheetDocument.AddWorkbookPart();
+            workbookpart.Workbook = new Workbook();
+
+            // Add a WorksheetPart to the WorkbookPart.  
+            WorksheetPart worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
+            worksheetPart.Worksheet = new Worksheet(new SheetData());
+
+            // Add Sheets to the Workbook.  
+            Sheets sheets = spreadsheetDocument.WorkbookPart.Workbook.
+                AppendChild<Sheets>(new Sheets());
+
+            // Append a new worksheet and associate it with the workbook.  
+            Sheet sheet = new Sheet()
+            {
+                Id = spreadsheetDocument.WorkbookPart.
+                GetIdOfPart(worksheetPart),
+                SheetId = 1,
+                Name = "mySheet"
+            };
+            sheets.Append(sheet);
+
+            Worksheet worksheet = new Worksheet();
+            SheetData sheetData = new SheetData();
+            Row row = new Row();
+            Cell cell = new Cell()
+            {
+                CellReference = "A1",
+                DataType = CellValues.Date,
+                CellValue = new CellValue(date)
+            };
+            row.Append(cell);
+            sheetData.Append(row);
+            worksheet.Append(sheetData);
+            worksheetPart.Worksheet = worksheet;
+
+            spreadsheetDocument.Close();
+        }
+    }
+}
